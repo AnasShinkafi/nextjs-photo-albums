92797fa949834e1bc2179fec498d2823f14fbcbb
Anas <anasisahskf59@gmail.com>
2023-09-18 09:56:02 +0100

    photo album

---
+++ app/gallery/GalleryGrid.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { SearchResult } from "./page";
+import { CloudinaryImage } from "./CloudinaryImage";
+import { ImageGrid } from "../../components/ImageGrid";
+
+export default function GalleryGrid({ images }: { images: SearchResult[] }) {
+  return (
+    <ImageGrid
+      images={images}
+      getImage={(imageData: SearchResult) => {
+        return (
+          <CloudinaryImage
+            key={imageData.public_id}
+            imageData={imageData}
+            width="400"
+            height="300"
+            alt="an image of something"
+          />
+        );
+      }}
+    />
+  );
+}
---
+++ components/ui/imageMenu.tsx
@@ -0,0 +1,49 @@
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import Link from "next/link";
+import { Pencil } from "lucide-react";
+import { Button } from "./button";
+import { SearchResult } from "../../app/gallery/page";
+import { AddToAlbumDialog } from "../AddToAlbumDialog";
+import { Menu } from "../icons/Menu";
+
+export function ImageMenu({ image }: { image: SearchResult }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="absolute top-2 right-2">
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" className="w-8 h-8 p-0">
+            <Menu />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40">
+          <DropdownMenuItem asChild>
+            <AddToAlbumDialog image={image} onClose={() => setOpen(false)} />
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Button
+              className="cursor-pointer flex justify-start pl-4"
+              asChild
+              variant="ghost"
+            >
+              <Link
+                href={`/edit?publicId=${encodeURIComponent(image.public_id)}`}
+              >
+                <Pencil className="mr-2 w-4 h-4" />
+                Edit
+              </Link>
+            </Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
